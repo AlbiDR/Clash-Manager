c449f7efccb0268fc4574770715971a05fbd3d0a
AlbiDR <aalbi97@gmail.com>
2026-01-07 05:13:38 +0100

    fix: import onMounted in NetworkSettings

--- Frontend-PWA/src/components/settings/NetworkSettings.vue
+++ Frontend-PWA/src/components/settings/NetworkSettings.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, computed, watch } from "vue";
+import { ref, computed, watch, onMounted } from "vue";
 import { useApiState } from "../../composables/useApiState";
 import Icon from "../Icon.vue";
 
