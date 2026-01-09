5924d9b6c2b69afeced5f6066656ac0b5f18215f
AlbiDR <aalbi97@gmail.com>
2026-01-09 02:01:57 +0100

    feat: Improve PullToRefresh indicator icon

--- Frontend-PWA/src/components/PullToRefresh.vue
+++ Frontend-PWA/src/components/PullToRefresh.vue
@@ -100,7 +100,21 @@ const indicatorClass = computed(() =>
   <div :class="indicatorClass" :style="indicatorStyle">
     <div class="ptr-content">
       <div v-if="refreshing" class="ptr-spinner"></div>
-      <div v-else class="ptr-icon" :style="iconStyle">⬇️</div>
+      <div v-else class="ptr-icon" :style="iconStyle">
+        <svg
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2.5"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        >
+          <line x1="12" y1="5" x2="12" y2="19"></line>
+          <polyline points="19 12 12 19 5 12"></polyline>
+        </svg>
+      </div>
     </div>
   </div>
 </template>
