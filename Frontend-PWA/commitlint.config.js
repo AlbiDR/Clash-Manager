7a28b92f9aa56b6f16ba50b647da25d3bd2a723b
AlbiDR <aalbi97@gmail.com>
2026-01-09 01:30:22 +0100

    feat(Frontend-PWA): setup commitlint

---
+++ Frontend-PWA/commitlint.config.js
@@ -0,0 +1,7 @@
+export default {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    // Optional: Add custom overrides here if needed
+    // "type-enum": [2, "always", ["feat", "fix", "docs", "style", "refactor", "perf", "test", "build", "ci", "chore", "revert"]],
+  },
+};
