0f448125d8c8badeedd302a2654d2f6c417280de
AlbiDR <aalbi97@gmail.com>
2026-01-09 01:13:03 +0100

    refactor: use `defineProps` without `const props =` in component scripts

--- Frontend-PWA/src/components/SkeletonSettingsCard.vue
+++ Frontend-PWA/src/components/SkeletonSettingsCard.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { computed } from "vue";
 
-const props = const props = defineProps<{
+const props = defineProps<{
   index?: number; // For subtle variations
 }>();
 
