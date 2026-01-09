e6a7ea91d105634facee62fad33d4bd089b76287
AlbiDR <aalbi97@gmail.com>
2026-01-09 02:01:57 +0100

    feat: Remove commitlint tooling

--- Frontend-PWA/commitlint.config.js
+++
@@ -1,7 +0,0 @@
-export default {
-  extends: ["@commitlint/config-conventional"],
-  rules: {
-    // Optional: Add custom overrides here if needed
-    // "type-enum": [2, "always", ["feat", "fix", "docs", "style", "refactor", "perf", "test", "build", "ci", "chore", "revert"]],
-  },
-};
