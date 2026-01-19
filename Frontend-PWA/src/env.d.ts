313e505002086e8209d6d3e037da41d6a2fce525
AlbiDR <97815338+AlbiDR@users.noreply.github.com>
2026-01-19 03:45:02 +0100

    Add VITE_WORKER_URL to ImportMetaEnv definition
    Add VITE_WORKER_URL to ImportMetaEnv definition

--- Frontend-PWA/src/env.d.ts
+++ Frontend-PWA/src/env.d.ts
@@ -5,6 +5,7 @@
 
 interface ImportMetaEnv {
   readonly VITE_GAS_URL: string;
+  readonly VITE_WORKER_URL: string;
   readonly VITE_APP_VERSION: string;
   readonly TEST: boolean;
   BASE_URL: string;
