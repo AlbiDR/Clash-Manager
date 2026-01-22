a2a703f05e2ceaf84158aac3d1e80966bc9d8d29
AlbiDR <97815338+AlbiDR@users.noreply.github.com>
2026-01-22 03:23:44 +0100

    Add declarations for GAS globals and namespace
    Add declarations for GAS globals and namespace

--- Backend-GAS/Orchestrator.ts
+++ Backend-GAS/Orchestrator.ts
@@ -17,6 +17,34 @@ import type { AppUtils } from "./Utilities";
 // @ts-ignore
 const VER_ORCHESTRATOR = "11.0.0";
 
+declare var SpreadsheetApp: any;
+declare var LockService: any;
+declare var PropertiesService: any;
+declare var UrlFetchApp: any;
+declare var CacheService: any;
+declare var ContentService: any;
+declare var Utilities: any;
+declare var ScriptApp: any;
+declare var Logger: any;
+declare var module: any;
+
+declare namespace GoogleAppsScript {
+  export namespace Events {
+    export type DoGet = any;
+    export type DoPost = any;
+    export type AppsScriptEvent = any;
+    export type SheetsOnEdit = any;
+  }
+  export namespace Spreadsheet {
+    export type Sheet = any;
+    export type Spreadsheet = any;
+    export type Range = any;
+  }
+  export namespace Content {
+    export type TextOutput = any;
+  }
+}
+
 // Global Declarations for GAS Environment
 declare const CONFIG: AppConfig;
 declare const Utils: AppUtils;
