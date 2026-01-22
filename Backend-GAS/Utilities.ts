691d392f6852c95cdf0f938a755201a88d5d395d
AlbiDR <97815338+AlbiDR@users.noreply.github.com>
2026-01-22 03:21:08 +0100

    Add declarations for GAS globals and module
    Add declarations for GAS globals and module

--- Backend-GAS/Utilities.ts
+++ Backend-GAS/Utilities.ts
@@ -23,6 +23,34 @@ import type { AppConfig } from "./Configuration";
 // @ts-ignore
 const VER_UTILITIES = "11.0.0";
 
+declare var SpreadsheetApp: any;
+declare var LockService: any;
+declare var PropertiesService: any;
+declare var UrlFetchApp: any;
+declare var CacheService: any;
+declare var ContentService: any;
+declare var Utilities: any;
+declare var ScriptApp: any;
+declare var Logger: any;
+declare var module: any;
+
+declare namespace GoogleAppsScript {
+  export namespace Events {
+    export type DoGet = any;
+    export type DoPost = any;
+    export type AppsScriptEvent = any;
+    export type SheetsOnEdit = any;
+  }
+  export namespace Spreadsheet {
+    export type Sheet = any;
+    export type Spreadsheet = any;
+    export type Range = any;
+  }
+  export namespace Content {
+    export type TextOutput = any;
+  }
+}
+
 // Global CONFIG and other GAS services declaration
 declare const CONFIG: AppConfig;
 
