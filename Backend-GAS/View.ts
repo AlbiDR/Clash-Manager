7db8fd381497a53d3f0edbb195afa12d271c4198
AlbiDR <aalbi97@gmail.com>
2026-02-02 17:25:34 +0100

    style: match status bar borders with background color for perfect invisibility

--- Backend-GAS/View.ts
+++ Backend-GAS/View.ts
@@ -203,9 +203,9 @@ var View: IView = {
         { 
           updateBorders: { 
             range: { sheetId, startRowIndex: 0, endRowIndex: 1, startColumnIndex: 0, endColumnIndex: totalCols }, 
-            innerVertical: { style: "SOLID", color: { red: 1, green: 1, blue: 1 } },
-            top: { style: "SOLID", color: { red: 1, green: 1, blue: 1 } },
-            bottom: { style: "SOLID", color: { red: 1, green: 1, blue: 1 } }
+            innerVertical: { style: "SOLID", color: statusBgRgb },
+            top: { style: "SOLID", color: statusBgRgb },
+            bottom: { style: "SOLID", color: statusBgRgb }
           } 
         },
 
