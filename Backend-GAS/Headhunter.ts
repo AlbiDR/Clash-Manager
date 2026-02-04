b572bcdd537fe979ad9fca852965ebdb32cd9b96
AlbiDR <aalbi97@gmail.com>
2026-02-04 13:08:22 +0100

    chore(release): bump Headhunter version to 12.2.0

--- Backend-GAS/Headhunter.ts
+++ Backend-GAS/Headhunter.ts
@@ -17,7 +17,7 @@ declare function refreshWebPayload(): void;
  *    Orchestrates: Strategy -> Store -> Scanner -> View.
  * ============================================================================
  */
-const VER_HEADHUNTER = "12.1.21";
+const VER_HEADHUNTER = "12.2.0";
 
 export interface IHeadhunter {
   scout(): void;
