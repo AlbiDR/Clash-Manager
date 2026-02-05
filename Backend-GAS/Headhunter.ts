9b0b75e1affe516f30b8d794f0cb540def21f944
AlbiDR <aalbi97@gmail.com>
2026-02-05 01:44:18 +0100

    chore: synchronize internal version string to 14.1.5 and finalize audit

--- Backend-GAS/Headhunter.ts
+++ Backend-GAS/Headhunter.ts
@@ -17,7 +17,7 @@ declare function refreshWebPayload(): void;
  *    Orchestrates: Strategy -> Store -> Scanner -> View.
  * ============================================================================
  */
-const VER_HEADHUNTER = "14.1.1";
+const VER_HEADHUNTER = "14.1.5";
 
 export interface IHeadhunter {
   scout(): void;
