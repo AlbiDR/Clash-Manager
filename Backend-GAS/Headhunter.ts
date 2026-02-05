d524f4c671a35298992fb9a35ad5f2291169203d
AlbiDR <aalbi97@gmail.com>
2026-02-05 02:13:10 +0100

    chore(hh): sync versioning to 14.2.3 and finalize hierarchy

--- Backend-GAS/Headhunter.ts
+++ Backend-GAS/Headhunter.ts
@@ -17,7 +17,7 @@ declare function refreshWebPayload(): void;
  *    Orchestrates: Strategy -> Store -> Scanner -> View.
  * ============================================================================
  */
-const VER_HEADHUNTER = "14.2.2";
+const VER_HEADHUNTER = "14.2.3";
 
 export interface IHeadhunter {
   scout(): void;
